1d432873ad75972fec21262d301ac648201417a0
ClaytonDewey <clay@dryadmedia.com>
2022-09-22 21:36:13 -0500

    Add and style social icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,22 @@
-import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
@@ -39,16 +51,64 @@ const Sidebar = () => {
       <ul>
         <li>
           <a
+            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
             target="_blank"
             rel="noreferrer"
-            href="https://www.linkedin.com/in/claytondewey/"
           >
-            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/bobangajicsm"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
           </a>
         </li>
       </ul>
+      {/* <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      /> */}
     </div>
   )
 }
 
 export default Sidebar
+// https://www.youtube.com/watch?v=bmpI252DmiI 20:39
+// https://github.com/bobangajicsm/react-portfolio-website/blob/master/src/components/Sidebar/index.js
