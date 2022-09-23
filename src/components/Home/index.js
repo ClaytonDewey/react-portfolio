0b727479724a9455c98edc77b19da15e0539f5ae
ClaytonDewey <clay@dryadmedia.com>
2022-09-23 15:44:59 -0500

    Import Logo component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
@@ -61,6 +62,7 @@ const Home = () => {
           CONTACT ME
         </Link>
       </div>
+      <Logo />
     </div>
   )
 }
