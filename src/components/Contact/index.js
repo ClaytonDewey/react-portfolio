970d9d64ca3efec609e5a02c6a7ac880d805ff72
ClaytonDewey <clay@dryadmedia.com>
2022-09-25 14:17:57 -0500

    Update contact form to send email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,40 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        (err) => {
+          console.error(err)
+          // alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <>
@@ -29,32 +53,39 @@ const Contact = () => {
             questions, don't hesitate to contact me using below form either.
           </p>
           <div className="contact-form">
-            <ul>
-              <li className="half">
-                <input type="text" name="name" placeholder="Name" required />
-              </li>
-              <li className="half">
-                <input type="email" name="email" placeholder="Email" required />
-              </li>
-              <li>
-                <input
-                  placeholder="Subject"
-                  type="text"
-                  name="subject"
-                  required
-                />
-              </li>
-              <li>
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  required
-                ></textarea>
-              </li>
-              <li>
-                <input type="submit" className="flat-button" value="SEND" />
-              </li>
-            </ul>
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
           </div>
         </div>
       </div>
