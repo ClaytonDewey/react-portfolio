96df7201002f654596bf46b06b306dc091d2e29c
ClaytonDewey <clay@dryadmedia.com>
2022-09-26 10:36:37 -0500

    Create Login page

---
+++ src/components/Login/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import { signInWithGoogle } from '../../firebase'
+
+const Login = () => {
+  return (
+    <div className="dashboard">
+      <button onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
+  )
+}
+
+export default Login
