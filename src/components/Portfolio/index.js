18c17e3637e2caeff7e5973529bf1c55f313bd48
ClaytonDewey <clay@dryadmedia.com>
2022-09-26 12:33:40 -0500

    Update to query firebase for Portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,10 +2,13 @@ import { useState, useEffect } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import Loader from 'react-loaders'
-import portfolioData from '../../data/portfolio.json'
+// import portfolioData from '../../data/portfolio.json'
+import { getDocs, collection } from 'firebase/firestore/lite'
+import { db } from '../../firebase'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const [portfolio, setPortfolio] = useState([])
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -17,6 +20,16 @@ const Portfolio = () => {
     }
   }, [])
 
+  useEffect(() => {
+    getPortfolio()
+  }, [])
+
+  const getPortfolio = async () => {
+    const querySnapshot = await getDocs(collection(db, 'portfolio'))
+    console.log(querySnapshot)
+    setPortfolio(querySnapshot.docs.map((doc) => doc.data()))
+  }
+
   const renderPortfolio = (portfolio) => {
     return (
       <div className="images-container">
@@ -24,12 +37,12 @@ const Portfolio = () => {
           return (
             <div className="image-box" key={idx}>
               <img
-                src={port.cover}
+                src={port.image}
                 className="portfolio-image"
                 alt="portfolio"
               />
               <div className="content">
-                <p className="title">{port.title}</p>
+                <p className="title">{port.name}</p>
                 <h2 className="description">{port.description}</h2>
                 <button className="btn" onClick={() => window.open(port.url)}>
                   View
@@ -52,7 +65,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        <div>{renderPortfolio(portfolioData.portfolio)}</div>
+        <div>{renderPortfolio(portfolio)}</div>
       </div>
       <Loader type="pacman" />
     </>
