b92f9c31f6e0070f72be29c6f0f5c1f68249c044
ClaytonDewey <clay@dryadmedia.com>
2022-09-26 10:03:36 -0500

    Create firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAv4lyM_typQc7HlcL8Nnc-Bzfe0hWhpr8',
+  authDomain: 'react-portfolio-dashboar-268d9.firebaseapp.com',
+  projectId: 'react-portfolio-dashboar-268d9',
+  storageBucket: 'react-portfolio-dashboar-268d9.appspot.com',
+  messagingSenderId: '374198619100',
+  appId: '1:374198619100:web:7284425dc73e7297885d19',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
